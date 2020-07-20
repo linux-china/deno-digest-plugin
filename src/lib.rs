60cae80672a90dfc54275af3671f3d235180c5f7
linux_china <libing.chen@gmail.com>
2020-07-19 23:46:10 -0700

    lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+use calcite;
+
+#[calcite::deno_op]
+fn multiply(a: f64, b: f64) -> f64 {
+    multiply_impl(a, b)
+}
+
+fn multiply_impl(a: f64, b: f64) -> f64 {
+    a * b
+}
+
+calcite::export!(multiply);
+
+#[cfg(test)]
+mod tests {
+    use crate::multiply_impl;
+
+    #[test]
+    fn test_multiply() {
+        let mut a = 2.0_f64;
+        let mut b = 2.0_f64;
+        let result = multiply_impl(a, b);
+        print!("{}", result);
+    }
+}
